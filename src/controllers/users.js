9f01018d600a5cb9a697a961e24486d9a8ce4d56
Oussama Harrathi <oussamaharrathi333@gmail.com>
2024-01-03 19:55:58 +0100

    changing gmail service to sedinblue 1

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -56,7 +56,7 @@ async function sendEmail(email, subject, text) {
     secure: false, 
     auth: {
       user: "harrathioussama8@gmail.com",
-      pass: "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"
+      pass: "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"
     }
   });
 
